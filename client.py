e23955d2e97903618d931c68c660d1c25011470b
Daniel Guerrero <danielgdevil@gmail.com>
2023-08-19 16:40:40 -0500

    Turn_logic_update
    Se ha podido POR FIN! aplicar la lógica de turnos, falta pulirla más pero al menos en este refactoring se esta respetando eso, posiblemente nos quedemos con el refactoring al final de cuentas, aun falla el inicio de partida con respecto a los colores y ciertos errores con el avance de fichas, no he tenido oportunidad de probar las updates de heavens_path y eat_function

--- client.py
+++ client.py
@@ -2,6 +2,10 @@ import zmq
 import time
 import random
 
+ZONE_SIZE = 14
+SAFE_ZONE_POSITIONS = [7, 14]
+SAFE_ZONE_OFFSET = 5
+
 def main():
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
@@ -14,39 +18,46 @@ def main():
     print(response)
 
     while True:
-        input("Press Enter to roll the dice...")
-        dice_roll1 = random.randint(1, 6)
-        dice_roll2 = random.randint(1, 6)
-        print("You rolled {} and {}.".format(dice_roll1, dice_roll2))
+        socket.send_json({"action": "get_turn"})  # Request the current turn from the server
+        current_turn = socket.recv_string()  # Receive the current turn from the server
 
-        decision = input("Do you want to move 1 piece or 2 pieces? (Type '1' or '2'): ")
-        if decision.strip() == "1":
-            piece_index = int(input("Choose a piece to move (0-3): "))
-            message = {
-                "action": "move",
-                "client_id": client_id,
-                "piece_indices": [piece_index],
-                "dice_roll1": dice_roll1,
-                "dice_roll2": dice_roll2
-            }
-        elif decision.strip() == "2":
-            piece_index1 = int(input("Choose the first piece to move (0-3): "))
-            piece_index2 = int(input("Choose the second piece to move (0-3, different from the first piece): "))
-            message = {
-                "action": "move",
-                "client_id": client_id,
-                "piece_indices": [piece_index1, piece_index2],
-                "dice_roll1": dice_roll1,
-                "dice_roll2": dice_roll2
-            }
+        if current_turn == client_id:
+            input("Press Enter to roll the dice...")
+            dice_roll1 = random.randint(1, 6)
+            dice_roll2 = random.randint(1, 6)
+            print("You rolled {} and {}.".format(dice_roll1, dice_roll2))
+
+            decision = input("Do you want to move 1 piece or 2 pieces? (Type '1' or '2'): ")
+            if decision.strip() == "1":
+                piece_index = int(input("Choose a piece to move (0-3): "))
+                message = {
+                    "action": "move",
+                    "client_id": client_id,
+                    "piece_indices": [piece_index],
+                    "dice_roll1": dice_roll1,
+                    "dice_roll2": dice_roll2
+                }
+            elif decision.strip() == "2":
+                piece_index1 = int(input("Choose the first piece to move (0-3): "))
+                piece_index2 = int(input("Choose the second piece to move (0-3, different from the first piece): "))
+                message = {
+                    "action": "move",
+                    "client_id": client_id,
+                    "piece_indices": [piece_index1, piece_index2],
+                    "dice_roll1": dice_roll1,
+                    "dice_roll2": dice_roll2
+                }
+            else:
+                print("Invalid input. Please enter '1' or '2'.")
+                continue
+
+            socket.send_json(message)
+            response = socket.recv_json()
+            show_positions(client_id, response["positions"], response["heaven_pieces"])
         else:
-            print("Invalid input. Please enter '1' or '2'.")
-            continue
+            print("Wait for your turn...")
 
-        socket.send_json(message)
-        response = socket.recv_json()
-        show_positions(client_id, response["positions"], response["heaven_pieces"])
-        time.sleep(1)
+        time.sleep(1)  # Wait for a short period before sending another turn request
 
 def show_positions(client_id, positions, heaven_pieces):
     zone_names = ["Red", "Blue", "Green", "Yellow"]
--- server.py
+++ server.py
@@ -11,29 +11,35 @@ def main():
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:5555")
 
-    players = {}
+    players = []
+    current_turn = 0
 
     while True:
         message = socket.recv_json()
 
         if message["action"] == "register":
             client_id = message["client_id"]
-            players[client_id] = {
-                "positions": [0, 0, 0, 0],
-                "heaven_pieces": 0,
-                "consecutive_even": 0,
-                "last_even": False
-            }
-            socket.send_string("Registered successfully!")
+            if len(players) < 4:
+                players.append({
+                    "id": client_id,
+                    "positions": [0, 0, 0, 0],
+                    "heaven_pieces": 0,
+                    "consecutive_even": 0,
+                    "last_even": False,
+                    "color": len(players)  # Assign a color based on the number of registered players
+                })
+                socket.send_string("Registered successfully!")
+            else:
+                socket.send_string("The game is full. Cannot register more players.")
 
         elif message["action"] == "move":
             client_id = message["client_id"]
-            target_player = players.get(client_id)
-            if target_player:
+            if players[current_turn]["id"] == client_id:
+                target_player = next(player for player in players if player["id"] == client_id)
                 piece_indices = message.get("piece_indices", [])
                 dice_roll1 = message["dice_roll1"]
                 dice_roll2 = message["dice_roll2"]
-                
+
                 total_roll = dice_roll1 + dice_roll2
                 new_position = total_roll
                 if target_player["last_even"]:
@@ -42,18 +48,21 @@ def main():
                     new_position += target_player["positions"][piece_indices[0]]
                 else:
                     new_position += target_player["positions"][piece_indices[0]]
-                    new_position += target_player["positions"][piece_indices[1]]
-                
+                    if len(piece_indices) > 1:
+                        new_position += target_player["positions"][piece_indices[1]]
+
                 if total_roll % 2 == 0:
                     target_player["consecutive_even"] += 1
                     if target_player["consecutive_even"] == MAX_CONSECUTIVE_EVEN:
                         max_position = max(new_position - total_roll, target_player["positions"][piece_indices[0]])
-                        max_position = max(max_position, target_player["positions"][piece_indices[1]])
+                        if len(piece_indices) > 1:
+                            max_position = max(max_position, target_player["positions"][piece_indices[1]])
                         new_position = check_eat(target_player["positions"], max_position)
                         new_position = check_zone(new_position)
                         new_position = check_safe_zone(new_position)
                         target_player["positions"][piece_indices[0]] = new_position
-                        target_player["positions"][piece_indices[1]] = new_position
+                        if len(piece_indices) > 1:
+                            target_player["positions"][piece_indices[1]] = new_position
                         socket.send_json({"positions": target_player["positions"], "heaven_pieces": target_player["heaven_pieces"]})
                     else:
                         target_player["last_even"] = True
@@ -61,7 +70,8 @@ def main():
                         new_position = check_zone(new_position)
                         new_position = check_safe_zone(new_position)
                         target_player["positions"][piece_indices[0]] = new_position
-                        target_player["positions"][piece_indices[1]] = new_position
+                        if len(piece_indices) > 1:
+                            target_player["positions"][piece_indices[1]] = new_position
                         socket.send_json({"positions": target_player["positions"], "heaven_pieces": target_player["heaven_pieces"]})
                 else:
                     target_player["consecutive_even"] = 0
@@ -70,19 +80,42 @@ def main():
                     new_position = check_zone(new_position)
                     new_position = check_safe_zone(new_position)
                     target_player["positions"][piece_indices[0]] = new_position
-                    target_player["positions"][piece_indices[1]] = new_position
+                    if len(piece_indices) > 1:
+                        target_player["positions"][piece_indices[1]] = new_position
                     if check_win(target_player["positions"]):
                         target_player["heaven_pieces"] += 1
                         target_player["positions"][piece_indices[0]] = -1
-                        target_player["positions"][piece_indices[1]] = -1
+                        if len(piece_indices) > 1:
+                            target_player["positions"][piece_indices[1]] = -1
                     socket.send_json({"positions": target_player["positions"], "heaven_pieces": target_player["heaven_pieces"]})
+                current_turn = (current_turn + 1) % len(players)
             else:
-                socket.send_string("Player {} not registered.".format(client_id))
-
+                socket.send_string("Not your turn.")    
+        elif message["action"] == "get_turn":
+            socket.send_string(players[current_turn]["id"])  # Send the current turn to the client
         else:
             socket.send_string("Unknown action.")
 
-# The rest of the code remains unchanged...
+def check_eat(positions, new_position):
+    for idx, pos in enumerate(positions):
+        if pos != 0 and pos == new_position:
+            return idx * -1
+    return new_position
+
+def check_zone(position):
+    if position >= ZONE_SIZE * 4:
+        return position - ZONE_SIZE * 4
+    return position
+
+def check_safe_zone(position):
+    zone_index = position // ZONE_SIZE
+    zone_position = position % ZONE_SIZE
+    if zone_position in SAFE_ZONE_POSITIONS and zone_position != SAFE_ZONE_OFFSET:
+        return position
+    return check_zone(position)
+
+def check_win(positions):
+    return all(pos == ZONE_SIZE * 4 + HEAVENS_PATH_LENGTH for pos in positions)
 
 if __name__ == "__main__":
     main()
