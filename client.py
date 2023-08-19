c72bf384869ddc0bd7e9c8f7d5cb58c495d755b7
Daniel Guerrero <danielgdevil@gmail.com>
2023-08-19 13:27:43 -0500

    piece_movement

--- client.py
+++ client.py
@@ -19,14 +19,30 @@ def main():
         dice_roll2 = random.randint(1, 6)
         print("You rolled {} and {}.".format(dice_roll1, dice_roll2))
         
-        piece_index = int(input("Choose a piece to move (0-3): "))
-        message = {
-            "action": "move",
-            "client_id": client_id,
-            "piece_index": piece_index,
-            "dice_roll1": dice_roll1,
-            "dice_roll2": dice_roll2
-        }
+        decision = input("Do you want to move 1 piece or 2 pieces? (Type '1' or '2'): ")
+        if decision.strip() == "1":
+            piece_index = int(input("Choose a piece to move (0-3): "))
+            message = {
+                "action": "move",
+                "client_id": client_id,
+                "piece_indices": [piece_index],
+                "dice_roll1": dice_roll1,
+                "dice_roll2": dice_roll2
+            }
+        elif decision.strip() == "2":
+            piece_index1 = int(input("Choose the first piece to move (0-3): "))
+            piece_index2 = int(input("Choose the second piece to move (0-3, different from the first piece): "))
+            message = {
+                "action": "move",
+                "client_id": client_id,
+                "piece_indices": [piece_index1, piece_index2],
+                "dice_roll1": dice_roll1,
+                "dice_roll2": dice_roll2
+            }
+        else:
+            print("Invalid input. Please enter '1' or '2'.")
+            continue
+        
         socket.send_json(message)
         response = socket.recv_json()
         print("Your new positions are: {}".format(response["positions"]))
--- server.py
+++ server.py
@@ -19,20 +19,29 @@ def main():
             client_id = message["client_id"]
             target_player = players.get(client_id)
             if target_player:
-                piece_index = message["piece_index"]
-                if 0 <= piece_index < 4:
-                    socket.send_string("Roll the dice. Type 'add' to add the results, or 'skip' to skip.")
-                    response = socket.recv_string()
-                    if response.strip().lower() == "add":
-                        dice_roll1 = message["dice_roll1"]
-                        dice_roll2 = message["dice_roll2"]
+                piece_indices = message.get("piece_indices", [])
+                dice_roll1 = message["dice_roll1"]
+                dice_roll2 = message["dice_roll2"]
+                
+                if len(piece_indices) == 1:
+                    piece_index = piece_indices[0]
+                    if 0 <= piece_index < 4:
                         total_roll = dice_roll1 + dice_roll2
                         target_player["positions"][piece_index] += total_roll
+                        socket.send_json({"positions": target_player["positions"]})
                     else:
-                        socket.send_string("Skipped adding dice results.")
-                    socket.send_json({"positions": target_player["positions"]})
+                        socket.send_string("Invalid piece index.")
+                elif len(piece_indices) == 2:
+                    piece_index1 = piece_indices[0]
+                    piece_index2 = piece_indices[1]
+                    if 0 <= piece_index1 < 4 and 0 <= piece_index2 < 4 and piece_index1 != piece_index2:
+                        target_player["positions"][piece_index1] += dice_roll1
+                        target_player["positions"][piece_index2] += dice_roll2
+                        socket.send_json({"positions": target_player["positions"]})
+                    else:
+                        socket.send_string("Invalid piece indices.")
                 else:
-                    socket.send_string("Invalid piece index.")
+                    socket.send_string("Invalid number of piece indices.")
             else:
                 socket.send_string("Player {} not registered.".format(client_id))
 
